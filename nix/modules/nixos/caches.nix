ddf401dd225865b1726f65e44d4ddb91d76e70ea
dscv103 <dvitrano@me.com>
2025-09-27 23:49:11 -0500

    Refactor nix settings for substituters and keys

--- nix/modules/nixos/caches.nix
+++ nix/modules/nixos/caches.nix
@@ -2,14 +2,16 @@
 # Do not edit without understanding overwrite policy.
 { lib, ... }:
 {
-  nix.settings = {
-    substituters = lib.mkBefore [
+ nix.settings = {
+    substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
     ];
-    trusted-public-keys = lib.mkBefore [
+    trusted-public-keys = [
+      # cache.nixos.org
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16EGsB+7Ra0/Rdk8="
+      # nix-community.cachix.org
       "nix-community.cachix.org-1:mNR2m8n0U6L0tJcHkJik4C59gz1+9a089/MefZP4V8g="
     ];
-  };
+  };  
 }
